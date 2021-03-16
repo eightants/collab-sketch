69b012edfc54499e23116fef4a584cbd443e9b34
Anthony <yihonganthony@gmail.com>
2021-03-16 15:30:02 -0500

    Added frontend files

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import { CollabCanvas } from "./components/Canvas";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <div className="whiteboard">
+      <CollabCanvas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Canvas.tsx
@@ -0,0 +1,37 @@
+import paper from "paper";
+import io from "socket.io-client";
+import React, { useEffect, useRef } from "react";
+
+export const CollabCanvas = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    let myPath: paper.Path;
+    const socket = io("http://localhost:3001");
+    const canvas = canvasRef.current;
+    paper.setup(canvas || "myCanvas");
+
+    paper.view.onMouseDown = (event: any) => {
+      myPath = new paper.Path();
+      myPath.strokeColor = new paper.Color(0, 0, 0);
+      myPath.add(event.point);
+    };
+
+    paper.view.onMouseDrag = (event: any) => {
+      myPath.add(event.point);
+    };
+
+    paper.view.onMouseUp = () => {
+      console.log(myPath);
+      socket.emit("drawPath", myPath);
+    };
+
+    socket.on("path", (path: paper.Path) => new paper.Path(path));
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  return <canvas id="myCanvas" ref={canvasRef} data-paper-resize></canvas>;
+};
