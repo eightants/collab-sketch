c0d034b957781b4ee8d97ba5351ee47750e95ebf
Anthony <yihonganthony@gmail.com>
2021-04-03 15:15:02 -0500

    Added home, lobby, draw page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
-import { CollabCanvas } from "./components/Canvas";
+import { Draw } from "./pages/Draw";
+import { Home } from "./pages/Home";
+import { Lobby } from "./pages/Lobby";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-      <div className="whiteboard">
-      <CollabCanvas />
-      </div>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/lobby">
+          <Lobby />
+        </Route>
+        <Route exact path="/draw">
+          <Draw />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Draw.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { CollabCanvas } from "../components/Canvas";
+
+export const Draw = () => {
+  return (
+    <div className="whiteboard">
+      <CollabCanvas />
+    </div>
+  );
+};
