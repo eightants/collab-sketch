16c414d330ea3712358a16c50e431258f8ad84b6
Anthony <yihonganthony@gmail.com>
2021-04-13 12:28:21 -0500

    App routing

--- server/app.js
+++ server/app.js
@@ -7,6 +7,10 @@ const buildPath = path.join(__dirname, '../client/', 'build');
 app.use(express.static(buildPath));
 const PORT = process.env.PORT || 3001;
 
+app.get('*', function(req,res) {
+  res.sendFile(path.join(buildPath, 'index.html'));
+});
+
 const server = http.createServer(app);
 
 const { InMemorySketchStore } = require("./sketchStore");
